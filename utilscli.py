798311f34cc9f899e42b1947fa1eabc58e266d57
Noah Gift <noah.gift@gmail.com>
2021-03-13 20:48:51 +0000

    adding cli interface to query API

--- utilscli.py
+++ utilscli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import click
 import mlib
-
+import requests
 
 @click.group()
 @click.version_option("1.0")
@@ -25,7 +25,17 @@ def retrain(tsize):
     click.echo(click.style(f"Retrained Model Name: {model_name}", bg="red", fg="white"))
 
 
-
+@cli.command("predict")
+@click.option("--weight", default=225, help="Weight to Pass In")
+@click.option("--host", default="http://localhost:8080/predict", help="Host to query")
+def mkrequest(weight, host):
+    """Sends prediction to ML Endpoint"""
+    
+    click.echo(click.style(f"Querying host {host} with weight: {weight}",
+        bg="green", fg="white"))
+    payload = {"Weight":weight}
+    result = requests.post(url=host, json=payload)
+    click.echo(click.style(f"result: {result.text}", bg="red", fg="white"))
 
 
 if __name__ == "__main__":
